43dcb6f1032f959252681bb748d6570bfbbbfa24
Ifra Shamim <ifrashamim29@gmail.com>
2025-02-04 13:15:52 +0500

    Done!

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -9,7 +9,6 @@ const Header = () => {
     const lists = [
         { name: 'Home', path: '/' },
         { name: 'About', path: '/about' },
-        // { name: 'Country', path: '/country' }
         { name: 'Contact', path: '/contact' }
     ];
 
